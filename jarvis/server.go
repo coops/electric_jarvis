3358fdbe631d85496f82e4218112862907a63ebb
cooper bethea <coops@foursquare.com>
2014-04-01 13:44:58 -0700

    Use an adapter struct to push oauth in/out of datastore.

--- jarvis/server.go
+++ jarvis/server.go
@@ -14,9 +14,43 @@ import (
 
 // datastore keyed by google id
 type User struct {
+	Email           string
+	OauthRandState  string
+	OauthToken      *oauth.Token
+	LatestCalendar  calendar.Calendar
+	datastoreHandle string
+}
+
+type UserFromDatastore struct {
 	Email          string
 	OauthRandState string
-	OauthToken     string
+	LatestCalendar calendar.Calendar
+	// b/c appengine can't store oauth.Token.Extra (map[string]string) we have to blow out all the fields
+	OauthAccessToken  string
+	OauthRefreshToken string
+	OauthExpiry       time.Time
+}
+
+func getUser(c appengine.Context, u *user.User) (*User, error) {
+	var dsuser UserFromDatastore
+	err := datastore.Get(c, makeUserKey(c, u), &dsuser)
+	if err != nil {
+		return nil, err
+	}
+	t := &oauth.Token{dsuser.OauthAccessToken, dsuser.OauthRefreshToken, dsuser.OauthExpiry, nil}
+	return &User{dsuser.Email, dsuser.OauthRandState, t, dsuser.LatestCalendar, u.ID}, nil
+}
+
+func setUser(c appengine.Context, u *user.User, user *User) error {
+	dsuser := &UserFromDatastore{user.Email, user.OauthRandState, user.LatestCalendar,
+		"", "", time.Now()}
+	if user.OauthToken != nil {
+		dsuser.OauthAccessToken = user.OauthToken.AccessToken
+		dsuser.OauthRefreshToken = user.OauthToken.RefreshToken
+		dsuser.OauthExpiry = user.OauthToken.Expiry
+	}
+	_, err := datastore.Put(c, makeUserKey(c, u), dsuser)
+	return err
 }
 
 func makeUserKey(c appengine.Context, u *user.User) *datastore.Key {
@@ -36,6 +70,7 @@ var config = &oauth.Config{
 func init() {
 	http.HandleFunc("/register", registerHandler)
 	http.HandleFunc("/oauth2callback", oauth2Callback)
+	http.HandleFunc("/read_calendar", readCalendarHandler)
 }
 
 func registerHandler(w http.ResponseWriter, r *http.Request) {
@@ -51,12 +86,14 @@ func registerHandler(w http.ResponseWriter, r *http.Request) {
 		w.WriteHeader(http.StatusFound)
 		return
 	}
-	datastoreKey := makeUserKey(c, u)
 	randState := fmt.Sprintf("st%d", time.Now().UnixNano())
-	record := User{Email: u.Email}
-	datastore.Get(c, datastoreKey, &record)
+	record, err := getUser(c, u)
+	if err != nil {
+		record = new(User)
+		record.Email = u.Email
+	}
 	record.OauthRandState = randState
-	handle, err := datastore.Put(c, datastoreKey, &record)
+	err = setUser(c, u, record)
 	if err != nil {
 		http.Error(w, err.Error(), 500)
 		return
@@ -71,10 +108,9 @@ func registerHandler(w http.ResponseWriter, r *http.Request) {
 func oauth2Callback(rw http.ResponseWriter, req *http.Request) {
 	c := appengine.NewContext(req)
 	u := user.Current(c)
-	datastoreKey := makeUserKey(c, u)
-	var record User
-	if err := datastore.Get(c, datastoreKey, &record); err != nil {
-		c.Infof("No datastore record for user: user = %#v, seeking key: %#v, error: %#v", u, datastoreKey, err)
+	record, err := getUser(c, u)
+	if err != nil {
+		c.Infof("No datastore record for user: user = %#v, error: %#v", u, err)
 		http.Error(rw, "", 500)
 		return
 	}
@@ -97,8 +133,20 @@ func oauth2Callback(rw http.ResponseWriter, req *http.Request) {
 			c.Criticalf("Token exchange error: %v", err)
 		}
 		fmt.Fprintf(rw, "<body>%s</body>", t.Token)
+		record.OauthToken = t.Token
+		setUser(c, u, record)
 		return
 	}
 	c.Infof("no code")
 	http.Error(rw, "", 500)
 }
+
+func readCalendarHandler(w http.ResponseWriter, r *http.Request) {
+	//	c := appengine.NewContext(r)
+	// look at my cal and liz's
+}
+
+// can actually use calendar push api: https://developers.google.com/google-apps/calendar/v3/push
+// to set up a watch on the calendar resources, then handle them as received from the app. although we might want to do a weekly full resync to make sure we don't drift.
+// so we'll be persisting 4 full calendars in datastore and updating them.
+// need to do a periodic refresh on the oauth token via cron
